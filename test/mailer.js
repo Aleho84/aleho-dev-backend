d7b79230fe9fc9a4174d3eba6ddc1abecb749877
Alejandro Abraham <alejandro.r.abraham@gmail.com>
2025-01-07 20:36:19 -0300

    update

--- test/mailer.js
+++
@@ -1,8 +0,0 @@
-import { sendCodeValidatorMail, generateCode } from '../utils/mailer.js';
-
-try {
-    const response = await sendCodeValidatorMail('alejandro.r.abraham@gmail.com', 'Alejandro', 'testAPP', 'https://nodemailer.com/nm_logo_200x136.png', generateCode(5));
-    console.log(response);
-} catch (error) {
-    console.log(error.message);    
-}
