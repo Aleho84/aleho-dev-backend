21cbf65b06deb339c9b4038e3309bab932a5bcec
Alejandro Abraham <alejandro.r.abraham@gmail.com>
2025-02-02 23:57:30 -0300

    add getSystemProcess

--- controllers/v1/serverController.js
+++ controllers/v1/serverController.js
@@ -1,10 +1,10 @@
-import { cp } from 'node:fs';
 import os from 'node:os';
+import { exec } from 'child_process';
 
 const getSystemInfo = () => {
     const freeMemory = os.freemem();
     const totalMemory = os.totalmem();
-    
+
     const cpu = os.cpus();
 
     const uptime = os.uptime();
@@ -18,13 +18,13 @@ const getSystemInfo = () => {
             B: totalMemory,
             KB: totalMemory / 1024,
             MB: totalMemory / 1024 / 1024,
-            GB: totalMemory / 1024 / 1024 /1024,
+            GB: totalMemory / 1024 / 1024 / 1024,
         },
         freeMemory: {
             B: freeMemory,
             KB: freeMemory / 1024,
             MB: freeMemory / 1024 / 1024,
-            GB: freeMemory / 1024 / 1024 /1024,
+            GB: freeMemory / 1024 / 1024 / 1024,
         },
         uptime: {
             totalSeconds: uptime,
@@ -38,13 +38,52 @@ const getSystemInfo = () => {
             cores: cpu.length,
             speed: cpu[0].speed,
             times: cpu[0].times
-        },               
+        },
     };
+};
+
+const getSystemProcess = async () => {
+    const command = process.platform === 'win32' ? 'tasklist' : 'ps aux';
+
+    try {
+        const { stdout, stderr } = await new Promise((resolve, reject) => {
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve({ stdout, stderr });
+                }
+            });
+        });
+
+        if (stderr) {
+            throw new Error(stderr);
+        }
+
+        const processes = stdout.split('\n').filter(line => line.trim() !== '');
+        const processObject = {};
+
+        processes.forEach((process, index) => {
+            processObject[`process_${index + 1}`] = process;
+        });
+
+        return processObject;
+    } catch (error) {
+        throw error;
+    }
 };
 
 export const systemInfo = async (req, res, next) => {
     try {
         res.status(200).json(getSystemInfo());
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const systemProcess = async (req, res, next) => {
+    try {
+        res.status(200).json(await getSystemProcess());
     } catch (error) {
         next(error);
     }
--- routes/v1/serverRouter.js
+++ routes/v1/serverRouter.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import passport from "passport";
-import { systemInfo } from "../../controllers/v1/serverController.js";
+import { systemInfo, systemProcess } from "../../controllers/v1/serverController.js";
 import { debuggerMidd } from "../../utils/debugger.js";
 
 const userRouter = Router();
 const auth = passport.authenticate("jwt", { session: false });
 
-userRouter.get("/systemInfo", debuggerMidd, systemInfo);
+userRouter.get("/systemInfo", debuggerMidd, auth, systemInfo);
+userRouter.get("/systemProcess", debuggerMidd, auth, systemProcess);
+
 
 export default userRouter;
